6cd0cf506f3eabf56f43e3fe27041e744b5f318e
hrjp <hrjp00@gmail.com>
2024-03-12 04:33:10 +0000

    Update installation paths for .sdf files and remove unused configuration file

---
+++ package_install.bash
@@ -0,0 +1,4 @@
+sudo sh -c 'echo "deb http://packages.osrfoundation.org/gazebo/ubuntu-stable `lsb_release -cs` main" > /etc/apt/sources.list.d/gazebo-stable.list'
+wget http://packages.osrfoundation.org/gazebo.key -O - | sudo apt-key add -
+sudo apt update
+sudo apt install -y libgz-sim7-dev ros-humble-ros-gzgarden
